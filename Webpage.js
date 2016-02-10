af809fdc97349435c40b0402a654870302d71ac0
Da74808n <d072192@Darrens-MacBook-Pro.local>
2016-02-09 19:22:10 -0500

    first commit

---
+++ Webpage.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<script src="Webpage.js"></script>
+<script>
+
+</script>
+</head>
+<body>
+    
+    
+    
+    <div class="nav">
+      <div class="container">
+        <ul class="pull-left">
+          <li><a href="HomePage1.html">Home</a></li>
+          <li><a href="AboutMe.html">Interesing Facts</a></li>
+          <li><a href="Lastly.html">Lastly</a></li>
+            <li<a href="Webpage.js">Webpage</a></li>
+        </ul>
+      
+      </div>
+    </div>
+
+    
+<p id="demo">A Paragraph.</p>
+<button type="button" onclick="myFunction()">Try it</button>
+
+
+
+    <section>
+        confirm("I feel awesome !");
+confirm("I am ready to go.");
+</section>
+    
+
+
+var iceCream = 'strawberry';
+if (iceCream === 'strawberry') {
+  alert('Yay, I love strawberry ice cream!');    
+} else {
+  alert('I will have vanilla too');    
+}
+
+
+
+
+</body>
+</html> 
